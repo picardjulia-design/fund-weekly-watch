9a704315d6385229c215c495aa739a7212896798
picardjulia-design <picardjulia@orange.fr>
2026-08-18 09:21:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,27 @@ st.dataframe(
     use_container_width=True,
     hide_index=True
 )
+st.divider()
+
+st.subheader("Importer les actualités de la semaine")
+
+uploaded_file = st.file_uploader(
+    "Dépose le fichier CSV de la semaine",
+    type=["csv"]
+)
+
+if uploaded_file is not None:
+    weekly_news = pd.read_csv(uploaded_file, sep=";")
+
+    weekly_news = weekly_news.dropna(
+        subset=["ticker", "url"],
+        how="any"
+    )
+
+    st.success("Fichier importé avec succès")
+
+    st.dataframe(
+        weekly_news,
+        use_container_width=True,
+        hide_index=True
+    )
